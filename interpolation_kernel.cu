d2a68bd6667b85180c855dd4fe94dd00df33691e
m.bobrin <m.bobrin@artistrazh.tech>
2022-08-10 15:36:20 +0000

    added cuda

--- interpolation_kernel.cu
+++ interpolation_kernel.cu
@@ -4,5 +4,8 @@ torch::Tensor trilinear_fw_cu(
     torch::Tensor feats,
     torch::Tensor point
 ) {
-    return feats;
+    const int N = feats.size(0), F = feats.size(2);
+    torch::Tensor feat_interp = torch::zeros({N, F}, feats.options());
+    const dim3 threads(16, 16) // 256
+    //TBA
 }
